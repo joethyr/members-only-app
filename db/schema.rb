fdaac18d5e10ec74b20e472296885a3759727d70
joethyr <joethayr@gmail.com>
2022-06-02 18:28:30 -0600

    update view with bootstrap css

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_25_175629) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_29_194234) do
+  create_table "events", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
